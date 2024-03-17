eb2c3066862beae107fe7697159e60beb51e0244
Robin Dong <robin.k.dong@gmail.com>
2024-03-17 14:55:24 +1100

    Align code in utils/ with pylint

--- utils/extract_tars.py
+++ utils/extract_tars.py
@@ -1,10 +1,10 @@
-import cv2
 import glob
 import tarfile
+from multiprocessing import Pool
 
+import cv2
 import numpy as np
 
-from multiprocessing import Pool
 
 n_jobs = 12
 input_path = "/mnt/backup_3080ti/cc12m/"
@@ -15,27 +15,29 @@ image_size = (256, 256)
 def task(tar_filename):
     drop_list = set()
     try:
-        tarf = tarfile.open(tar_filename)
-        for member in tarf.getmembers():
-            if member.name.endswith(".txt"):
-                txt_fp = tarf.extractfile(member)
-                content = txt_fp.read()
-                try:
-                    text = content.decode("ascii")
-                except Exception:
-                    drop_list.add(member.name[:-4])
-                    continue
-                with open(output_path + member.name, "w") as fp:
-                    fp.write(text)
-            if member.name.endswith(".jpg"):
-                if member.name[:-4] in drop_list:
-                    continue
-                image_fp = tarf.extractfile(member)
-                image_content = np.asarray(bytearray(image_fp.read()), dtype="uint8")
-                image = cv2.imdecode(image_content, cv2.IMREAD_COLOR)
-                image = cv2.resize(image, image_size)
-                cv2.imwrite(output_path + member.name, image)
-    except Exception as ex:
+        with tarfile.open(tar_filename) as tarf:
+            for member in tarf.getmembers():
+                if member.name.endswith(".txt"):
+                    txt_fp = tarf.extractfile(member)
+                    content = txt_fp.read()
+                    try:
+                        text = content.decode("ascii")
+                    except UnicodeDecodeError:
+                        drop_list.add(member.name[:-4])
+                        continue
+                    with open(output_path + member.name, "w", encoding="utf-8") as fp:
+                        fp.write(text)
+                if member.name.endswith(".jpg"):
+                    if member.name[:-4] in drop_list:
+                        continue
+                    image_fp = tarf.extractfile(member)
+                    image_content = np.asarray(
+                        bytearray(image_fp.read()), dtype="uint8"
+                    )
+                    image = cv2.imdecode(image_content, cv2.IMREAD_COLOR)
+                    image = cv2.resize(image, image_size)
+                    cv2.imwrite(output_path + member.name, image)
+    except OSError as ex:
         print("Exception:", ex)
         print("Failed filename:", tar_filename)
 
